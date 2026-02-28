2c975a8b1e96c3b0c42955e07850c17ecc64923c
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-27 22:13:20 -0300

    feat: upload files via Android Dev Tools

---
+++ nodejs-mobile-module/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.janeasystems.rn_nodejs_mobile"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions { jvmTarget = "17" }
+
+    sourceSets {
+        getByName("main") {
+            jniLibs.srcDirs("src/main/jniLibs")
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+}
